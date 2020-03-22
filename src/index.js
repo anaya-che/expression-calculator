513bd2543fad7cdeca5141f8d7696430f52b01bc
Anastasiya Chabatarova <anaya.che22@gmail.com>
2020-03-23 01:25:09 +0300

    fix: fix bugs

--- src/index.js
+++ src/index.js
@@ -4,11 +4,18 @@ function eval() {
 }
 
 function expressionCalculator(expr) {
-    let arr = expr.replace(/\s/g, '').split('');
-    let pattOper = /(\*|\/|\+|\-)/;
+    let expr1 = expr.replace(/\s/g, '').split(/(\*|\)|\/|\+|\(|\-)/);
+    let pattOper = /\*|\/|\+|\-/;
+    let pattOper1 = /(\*|\/|\+|\-)/;
     let pateNum = /\d/;
+    let pattBrack = /\(/;
     let operation = '';
     let exp = [];
+    let i = 0;
+    let brackOpen = (expr.match(/\(/g)) ? expr.match(/\(/g).length : 0;
+    let brackClose = (expr.match(/\)/g)) ? expr.match(/\)/g).length : 0;
+    let arr = expr1.filter((val) => (val !== ''))
+    if (brackOpen !== brackClose) { throw new Error("ExpressionError: Brackets must be paired");}
 
     function operators(exp) {
         if (operation === '+') {
@@ -24,26 +31,51 @@ function expressionCalculator(expr) {
             return res =  exp.reduce((a, b) => a / b);
         }
     }
-
-    while (arr.length !== 1) {
-        for (i = 0; i < arr.length; i++) {
-            if (pattOper.test(arr[i])) {
-            a = arr[i-1];
-            exp.push(parseFloat(a));
-            operation = arr[i];
-            b = arr[i+1];
-            if (arr[i] === '/' && arr[i+1] == 0) { throw new Error("TypeError: Division by zero.")}
-            exp.push(parseFloat(b));
-            res = operators(exp);
-            arr.splice((i-1),3);
-            arr.unshift(operators(exp));
-            i = 0;
-            exp = [];
-            } 
-        }
-    }
     
-    return res;
+
+    if (arr.length > 1) { 
+        let n = arr.length;
+        
+        for (i = 0; i < n; i++) {
+          (arr.indexOf("(") !== -1) ? i = (arr.lastIndexOf("(") + 2) : '';
+          (arr.indexOf(")") !== -1) ? n = (arr.indexOf(")", i) - 1) : '';
+          let f = n - i + 3;
+          if (arr.indexOf("/", i) !== -1 && arr.indexOf("/", i) < (n + 1)) {
+            (arr.indexOf("/", i) !== -1 && arr.indexOf("/", i) < (n + 1)) ? i = arr.indexOf("/", i) : '';
+          } else if (arr.indexOf("*", i) !== -1 && arr.indexOf("*", i) < (n + 1)) {
+            (arr.indexOf("*", i) !== -1 && arr.indexOf("*", i) < (n + 1)) ? i = arr.indexOf("*", i) : '';
+          }  
+              if (pattOper.test(arr[i])) {
+                a = arr[i-1];
+                exp.push(parseFloat(a));
+                operation = arr[i];
+                b = arr[i+1];
+                if (arr[i] === '/' && arr[i+1] == 0) { throw new Error("TypeError: Division by zero.")}
+                exp.push(parseFloat(b));
+                res = operators(exp);
+              if (arr.toString().match(/\(/g)) {
+                if (f === 4) {
+                  arr[i-2] = (operators(exp));
+                  arr.splice((i-1),4);
+                  i = 0;
+                  exp = [];
+                }
+                if (f > 4) {
+                  arr[i-1] = (operators(exp));
+                  arr.splice((i),2);
+                  exp = [];
+                  i = 0;
+                }
+              } else {
+                arr[i-1] = (operators(exp));
+                arr.splice((i),2);
+                i = 0;
+                exp = [];
+              } }
+          }
+      }
+      
+      return res;
 }
 
 module.exports = {
